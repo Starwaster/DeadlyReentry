61f7cd6c10f97d2f998505da975c6061126f487a
NathanKell <nathankell@gmail.com>
2014-07-20 04:15:54 -0400

    Update assembly info

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeadlyReentry")]
+[assembly: AssemblyTitle("DeadlyReentryContinued")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Multiversal Mechatronics")]
-[assembly: AssemblyProduct("DeadlyReentry")]
-[assembly: AssemblyCopyright("Copyright © Multiversal Mechatronics 2013")]
+[assembly: AssemblyCompany("NathanKell")]
+[assembly: AssemblyProduct("DeadlyReentryContinued")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("5.0.*")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
