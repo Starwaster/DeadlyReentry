129140337b50fe46040924aa4f0ff108f3dd9842
Starwaster <starwaster@gmail.com>
2016-07-05 23:42:16 -0400

    Updating versioning file
    * Updating versioning file

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.4.6")]
+[assembly: AssemblyVersion("7.4.7")]
 //[assembly: AssemblyFileVersion("7.0.*.*")]
 [assembly: KSPAssembly("DeadlyReentry", 1, 0)]
 //[assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
