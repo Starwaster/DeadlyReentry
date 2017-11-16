6cebade66b4d35dd905b3a0117806be39312231d
Starwaster <starwaster@gmail.com>
2017-11-16 16:22:18 -0500

    Update versioning and recompile
    * Update versioning and recompile for KSP 1.3.1
    * Adjusted heat shield lossConst and pyrolysisLossFactor to better balance survivability

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.6.1")]
+[assembly: AssemblyVersion("7.6.2")]
 //[assembly: AssemblyFileVersion("7.0.*.*")]
 [assembly: KSPAssembly("DeadlyReentry", 7, 6)]
 //[assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
